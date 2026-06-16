9868fd6775297e5bafc973c950ebe9ab68db1650
Gustavo Camargo <gustavo@segredes.com.br>
2026-06-15 21:17:14 -0300

    Teste commmit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        Pessoa gustavo = new Pessoa(1, "Gustavo", 20);
+        Pessoa gustavo = new Pessoa("1", "Gustavo", 20);
 
-        System.out.println("Gustavo: " + gustavo.getNome());
         System.out.println(gustavo.toString());
+        System.out.println("Hello World!");
     }
 }

7090c9cbe8b952e93e6903f5c9ff3ae7a0915192
Gustavo Camargo <gustavo@segredes.com.br>
2026-06-15 21:13:37 -0300

    feature: Create person gustavo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Pessoa gustavo = new Pessoa(1, "Gustavo", 20);
+
+        System.out.println("Gustavo: " + gustavo.getNome());
+        System.out.println(gustavo.toString());
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,45 @@
+public class Pessoa {
+
+    private String id;
+    private String nome;
+    private int idade;
+
+    public Pessoa(String id, String nome, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
